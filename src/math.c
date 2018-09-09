891bb74be324ccbe91fd3df9a3eed33243d5e907
unknown <mclemoret0@students.rowan.edu>
2018-09-09 19:40:12 -0400

    Added math function to 'math.c'

--- src/math.c
+++ src/math.c
@@ -15,3 +15,29 @@ int main(void) {
 	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
+
+
+int math( int num1, int num2, char Operator ) { //Function takes two integers and a character that will be the operator
+
+
+    switch(Operator) {
+          case '+' : //Addition
+             return num1 + num2;
+             break;
+          case '-' : //Subtraction
+             return num1 - num2;
+             break;
+          case '/' :
+             return num1 / num2;
+             break;
+          case '*' : //Multiplication
+          case 'x' : //Just in case the user uses different symbols for multiplication
+          case 'X' :
+             return num1 * num2;
+             break;
+          default :
+             printf("Invalid operation given" );
+             return NULL;
+             break
+       }
+}
